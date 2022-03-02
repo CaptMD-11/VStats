aa83eaa8221516cd121e5a96c768dcf70f7d4a8a
vnydhruva <vnydhruva@localhost>
2022-03-02 10:44:56 -0800

    added matrix multiplication by scalar method

--- VStats/src/MatrixOperations.java
+++ VStats/src/MatrixOperations.java
@@ -8,17 +8,19 @@ public class MatrixOperations {
 	}
 	
 	public static class MatrixData {
-		double[][] arr1;
-		double[][] arr2; 
+		double[][] matrix1;
+		double[][] matrix2; 
+		double[][] generalMatrix; 
+		double scalar; 
 	}
 	
-	public double[][] computeMatrixAddition(MatrixData data) { // arr1 & arr2 must have same dimensions 
+	public double[][] computeMatrixAddition(MatrixData data) { // matrix1 & matrix2 must have same dimensions 
 		
-		double[][] res = new double[data.arr1.length][data.arr1[0].length]; 
+		double[][] res = new double[data.matrix1.length][data.matrix1[0].length]; 
 		
-		for (int i = 0; i < data.arr1.length; i++) {
-			for (int j = 0; j < data.arr1[i].length; j++) {
-				res[i][j] = (data.arr1[i][j] + data.arr2[i][j]);
+		for (int i = 0; i < data.matrix1.length; i++) {
+			for (int j = 0; j < data.matrix1[i].length; j++) {
+				res[i][j] = (data.matrix1[i][j] + data.matrix2[i][j]);
 			}
 		}
 		
@@ -28,11 +30,25 @@ public class MatrixOperations {
 	
 	public double[][] computeMatrixSubtraction(MatrixData data) {
 		
-		double[][] res = new double[data.arr1.length][data.arr1[0].length]; 
+		double[][] res = new double[data.matrix1.length][data.matrix1[0].length]; 
 		
-		for (int i = 0; i < data.arr1.length; i++) {
-			for (int j = 0; j < data.arr1[i].length; j++) {
-				res[i][j] = (data.arr1[i][j] - data.arr2[i][j]);
+		for (int i = 0; i < data.matrix1.length; i++) {
+			for (int j = 0; j < data.matrix1[i].length; j++) {
+				res[i][j] = (data.matrix1[i][j] - data.matrix2[i][j]);
+			}
+		}
+		
+		return res; 
+		
+	}
+	
+	public double[][] computeMatrixMultiplicationByScalar(MatrixData data) {
+		
+		double[][] res = new double[data.generalMatrix.length][data.generalMatrix[0].length]; 
+		
+		for (int i = 0; i < data.generalMatrix.length; i++) {
+			for (int j = 0; j < data.generalMatrix[i].length; j++) {
+				res[i][j] = (data.scalar) * (data.generalMatrix[i][j]); 
 			}
 		}
 		
@@ -57,8 +73,10 @@ public class MatrixOperations {
 		
 		// enter your data below! 
 		
-		myData.arr1 = new double[][]{{2, 3}, {3, 4}};
-		myData.arr2 = new double[][]{{2,2}, {3,2}}; 
+		myData.matrix1 = new double[][] {{2, 3}, {3, 4}};
+		myData.matrix2 = new double[][] {{2,2}, {3,2}}; 
+		myData.generalMatrix = new double[][] {{3,4} , {2,3}}; 
+		myData.scalar = 2; 
 		
 		///////////////////////////////////////////////////////
 		
@@ -83,6 +101,15 @@ public class MatrixOperations {
 		}
 		
 		System.out.println();
+		
+		double[][] tempScalarMultiplication = myObj.computeMatrixMultiplicationByScalar(myData); 
+		System.out.println("Matrix subtraction result: "); 
+		for (int i = 0; i < tempScalarMultiplication.length; i++) {
+			for (int j = 0; j < tempScalarMultiplication[i].length; j++) {
+				System.out.print(tempScalarMultiplication[i][j] + " "); 
+			}
+			System.out.println(); 
+		}
 		
 		//System.out.println("Matrix addition result: " + myObj.computeMatrixAddition(myData)); 
 		

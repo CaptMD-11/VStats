28b3183ed3eef92ff8a1fbf8c4bf901593385843
vnydhruva <vnydhruva@localhost>
2022-03-03 15:08:10 -0800

    added some additional methods to MatrixOperations class

--- VStats/src/MatrixOperations.java
+++ VStats/src/MatrixOperations.java
@@ -1,5 +1,6 @@
+import java.util.ArrayList;
 
-public class MatrixOperations {
+public class MatrixOperations { 
 	
 	private int vars;
 	
@@ -12,6 +13,30 @@ public class MatrixOperations {
 		double[][] matrix2; 
 		double[][] generalMatrix; 
 		double scalar; 
+	}
+	
+	public double getRowSum(double[][] data, int row) {
+		
+		double sum = 0; 
+		
+		for (int i = 0; i < data[0].length; i++) {
+			sum += data[row][i]; 
+		}
+		
+		return sum; 
+		
+	}
+	
+	public double getColumnSum(double[][] data, int col) {
+		
+		double sum = 0;
+		
+		for (int i = 0; i < data.length; i++) {
+			sum += data[i][col]; 
+		}
+		
+		return sum; 
+		
 	}
 	
 	public double[][] computeMatrixAddition(MatrixData data) { // matrix1 & matrix2 must have same dimensions 
@@ -49,6 +74,27 @@ public class MatrixOperations {
 		for (int i = 0; i < data.generalMatrix.length; i++) {
 			for (int j = 0; j < data.generalMatrix[i].length; j++) {
 				res[i][j] = (data.scalar) * (data.generalMatrix[i][j]); 
+			}
+		}
+		
+		return res; 
+		
+	}
+	
+	public double[][] computeMatrixMultiplicationBySquareMatrices(MatrixData data) { // matrix1 * matrix2 
+		
+		double[][] res = new double[data.matrix1.length][data.matrix1[0].length]; 
+		ArrayList<Double> sums = new ArrayList<Double>(data.matrix1.length * data.matrix1[0].length);
+		
+		for (int i = 0; i < res.length; i++) {
+			for (int j = 0; j < res[i].length; j++) {
+				res[i][j] = getRowSum(data.matrix1, i) * getColumnSum(data.matrix2, j); 
+			}
+		}
+		
+		for (int i = 0; i < res.length; i++) {
+			for (int j = 0; j < res[i].length; j++) {
+				//res[i][j] = data.matrix1[i][]
 			}
 		}
 		
@@ -103,10 +149,21 @@ public class MatrixOperations {
 		System.out.println();
 		
 		double[][] tempScalarMultiplication = myObj.computeMatrixMultiplicationByScalar(myData); 
-		System.out.println("Matrix subtraction result: "); 
+		System.out.println("Matrix scalar multiplication result: "); 
 		for (int i = 0; i < tempScalarMultiplication.length; i++) {
 			for (int j = 0; j < tempScalarMultiplication[i].length; j++) {
 				System.out.print(tempScalarMultiplication[i][j] + " "); 
+			}
+			System.out.println(); 
+		}
+		
+		System.out.println(); 
+		
+		double[][] tempMatrixMultiplicationBySquareMatrices = myObj.computeMatrixMultiplicationBySquareMatrices(myData); 
+		System.out.println("Matrix multiplication by square matrices result: "); 
+		for (int i = 0; i < tempMatrixMultiplicationBySquareMatrices.length; i++) {
+			for (int j = 0; j < tempMatrixMultiplicationBySquareMatrices[i].length; j++) {
+				System.out.print(tempMatrixMultiplicationBySquareMatrices[i][j] + " "); 
 			}
 			System.out.println(); 
 		}

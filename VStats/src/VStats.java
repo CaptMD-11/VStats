284937bbe226057d4a85ee6cdc000758a7f07e8d
vnydhruva <vnydhruva@localhost>
2022-04-07 12:31:51 -0700

    removed some more classes...

--- VStats/src/VStats.java
+++ VStats/src/VStats.java
@@ -219,21 +219,11 @@ public class VStats {
 	}
 	
 	public int computeLength(double[] data) {
-		
-		int count = 0; 
-		
-		for (int i = 0; i < data.length; i++) {
-			count++; 
-		}
-		
-		return count; 
-		
+		return data.length; 
 	}
 	
 	public double atIndex(double[] data, int i) {
-		
 		return data[i]; 
-		
 	}
 	
 	public double computeIQR(double[] data) {
@@ -256,5 +246,186 @@ public class VStats {
 		return res; 
 		
 	}
+	
+	public int computeFactorial(int inputVal) {
+		
+		int resFactorial = 1;
+		
+	    for (int i = inputVal; i >= 1; i--) {
+	    	resFactorial = resFactorial * i;
+	    }
+	    return resFactorial;
+	}
 
+	public double computeCombination(int numTrials, int xVal) {
+		
+		return ((computeFactorial(numTrials)) / ( (computeFactorial(xVal)) * (computeFactorial(numTrials - xVal))) * 1.0);
+	}
+
+	public double computeBinomialPdf(int numTrials, int xVal, double pSuccess) {
+		return ((computeCombination(numTrials, xVal)) * (Math.pow(pSuccess, xVal)) * (Math.pow((1-pSuccess), (numTrials - xVal))) * 1.0);
+	}
+
+	public double computepBinomialPdf(int numTrials, int xVal, double pSuccess) {
+		
+		double pBinomialPdf = 0.0; 
+		
+		pBinomialPdf = (computeCombination(numTrials, xVal)) * (Math.pow(pSuccess, xVal)) * (Math.pow((1-pSuccess), (numTrials - xVal)));
+		
+		return pBinomialPdf; 
+		
+	}
+
+
+	public void computeBinomialCdf(int numTrials, int xVal, double pSuccess, int inputLBound , int inputHBound) {
+
+		for (int i = inputLBound; i <= inputHBound; i++) {
+			xVal = i; 
+			computepBinomialPdf(numTrials, xVal, pSuccess);
+		}
+
+	}
+	
+public double NormalPDF(double inputZ) {
+		
+		double constant = (1) / (Math.sqrt(2 * Math.PI)); 
+		double exponent = (Math.pow(inputZ, 2.0)) / (-2); 
+		
+		return (constant) * (Math.pow(Math.E, exponent)); 
+		
+	} 
+	
+	public double computeFiniteZProbLeftRiemann(double inputZLow, double inputZHigh) {
+		
+		double sum = 0.0; 
+		
+		//double increment = (inputZHigh - inputZLow)/(Math.pow(10, 7)); 
+		double increment = 1.0 / (Math.pow(10, 7)); 
+
+		for (double i = inputZLow; i < inputZHigh; i += increment) {
+			sum += ((NormalPDF(i)) * (increment)); 
+		}
+		
+		return sum; 
+	}
+	
+	public double computeFiniteZProbRightRiemann(double inputZLow, double inputZHigh) {
+		
+		double sum = 0.0; 
+		
+		//double increment = (inputZHigh - inputZLow)/(Math.pow(10, 7)); 
+		double increment = 1.0 / (Math.pow(10, 7)); 
+		
+		for (double i = (inputZLow + increment); i <= inputZHigh; i += increment) {
+			sum += (NormalPDF(i) * increment); 
+		}
+		
+		return sum; 
+		
+	}
+	
+	public double computeFiniteZProbAvgLeftRightRiemann(double inputZLow, double inputZHigh) {
+		return ((computeFiniteZProbRightRiemann(inputZLow, inputZHigh)) + 
+											(computeFiniteZProbLeftRiemann(inputZLow, inputZHigh))) / 2; 
+	}
+	
+	public double computeFiniteZProbMidpointRiemann(double inputZLow, double inputZHigh) {
+		
+		double sum = 0.0;
+		
+		//double increment = (inputZHigh - inputZLow)/(Math.pow(10, 7)); 
+		double increment = 1.0 / (Math.pow(10, 7)); 
+		
+		for (double i = (inputZLow + (increment/2)); i < inputZHigh; i+= increment) {
+			sum += (NormalPDF(i) * increment); 
+		}
+		
+		return sum; 
+		
+	}
+	
+	public double computeFiniteZProbTrapezoidRiemann(double inputZLow, double inputZHigh) {
+		
+		double sum = 0.0; 
+		
+		//double increment = (inputZHigh - inputZLow)/(Math.pow(10, 7)); 
+		double increment = 1.0 / (Math.pow(10, 7)); 
+				
+		for (double i = inputZLow; i < (inputZHigh-increment); i += increment) {
+			sum += ((0.5) * (NormalPDF(i) + NormalPDF(i + increment)) * (increment)); 
+		}
+		
+		return sum; 
+		
+	}
+	
+	public String computeInverseNormalApprox(double input) { // works, but highly inefficient 
+		// with respect to probability to the left of input value 
+		
+		if ((input < 0) || (input > 1))
+			return "invalid input"; 
+		else if (input == 0)
+			return "-∞"; 
+		else if (input == 1)
+			return "∞"; 
+		else {
+			double starter = 0.0; 
+			
+			while (starter <= 1) {
+				if (computeFiniteZProbMidpointRiemann(-100000.0, starter) == input) 
+					return starter + ""; 
+				starter += 0.0000001;
+			}
+
+		}
+		
+		return ""; 
+		
+	}
+	
+	public int computeRandomWholeNumber(int low, int high) {
+		return (int) (Math.random()*(high-low+1) + low); 
+	}
+	
+	public double computeRandomDecimalNumber(double low, double high) {
+		return Math.random()*(high-low+1) + low; 
+	}
+	
+	public double computePermutations(int numberOfObservations, int numberChoose) {
+		return (computeFactorial(numberOfObservations)) / (computeFactorial(numberOfObservations - numberChoose)); 
+	}
+	
+	public double computeDiscreteExpectedValue(double[] dataArray, double[] probabilitiesArray) {
+		
+		double sum = 0.0; 
+		
+		for (int i = 0; i < computeLength(dataArray); i++) {
+			sum += ((atIndex(dataArray, i)) * (atIndex(probabilitiesArray, i))); 
+		}
+		
+		return sum; 
+		
+	}
+	
+	public double computeDiscreteVariance(double[] dataArray, double[] probabilitiesArray) {
+		
+		double sum = 0.0; 
+		double meanOfX = computeDiscreteExpectedValue(dataArray, probabilitiesArray); 
+		
+		for (int i = 0; i < computeLength(dataArray); i++) {
+			
+			sum += ( (Math.pow((atIndex(dataArray, i) - meanOfX), 2)) * (atIndex(probabilitiesArray, i)) ); 
+			
+		}
+		
+		return sum; 
+		
+	}
+	
+	public double computeDiscreteStandardDeviation(double[] dataArray, double[] probabilitiesArray) {
+		
+		return Math.sqrt(computeDiscreteVariance(dataArray, probabilitiesArray)); 
+		
+	}
+	
 }

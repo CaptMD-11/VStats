4495a57d472ebce5415c8b446e78e3b5320e10ba
CaptMD-11 <86084719+CaptMD-11@users.noreply.github.com>
2022-06-12 12:30:16 -0700

    added LICENSE stuff

--- VStats/src/VStats.java
+++ VStats/src/VStats.java
@@ -1,8 +1,26 @@
+/*
+ * This is VStats, a Java library made by Vignesh Nydhruva, that computes various statistics. 
+    Copyright (C) 2022  Vignesh Nydhruva
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import java.util.ArrayList;
 
 public final class VStats {
 
-	public VStats() { 
+	private VStats() { 
 
 	} 
 

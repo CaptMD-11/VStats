c022cb7eb947398a6b55f714f43f02e04c062c70
CaptMD-11 <86084719+CaptMD-11@users.noreply.github.com>
2023-01-03 12:38:42 -0800

    refined VCSV class

--- Code/JavaCode/src/Runner.java
+++ Code/JavaCode/src/Runner.java
@@ -1,61 +1,11 @@
 public class Runner {
     public static void main(String[] args) {
-        // System.out.println(VStats.computeGammaFunction(0.5));
-        // System.out.println(VStats.computeChiSquarePDF(0, 3));
-        // System.out.println(VStats.computeChiSquareCDFRiemannSum(0.7, 5, 6));
-        // System.out.println(VStats.computeChiSquareCDFRiemannSum(0.01, 4.7, 5));
 
-        // System.out.println(VStats.computeGammaFunction(2.2345));
+        System.out.println(VCSV.getNumCols("menu.csv"));
 
-        // System.out.println(VStats.computeChiSquareCDF(2.02, 5.778, 7));
+        // double[] res = VCSV.getColumnOfNumbers("menu.csv", 7);
 
-        // System.out.println(VStats.StirlingTest2(2));
-
-        // System.out.println(VStats.computeChiSquareCDF(6, 100, 3));
-
-        // System.out.println(VStats.computeChiSquarePDF(0.00204, 1));
-
-        // double[] observed = { 4, 7, 8, 5, 6 };
-        // double[] expected = { 6, 6, 6, 6, 6 };
-
-        // System.out.println(VStats.computeChiSquareGOFTest(observed, expected, 0.05));
-
-        // double[][] obs = { { 30, 10 }, { 15, 25 }, { 15, 5 } };
-        // System.out.println(VStats.computeChiSquareHomogeneityTest(obs, 0.05));
-
-        // System.out.println(VStats.computeChiSquareCDF(0.00204, 100, 1));
-
-        // System.out.println(VStats.TEST(4));
-
-        // System.out.println(VStats.computeGammaFunctionNemesApprox(0.5));
-
-        // System.out.println(VStats.computeTPDF(0.2007, 8));
-
-        // System.out.println(VStats.computeTCDF(-100, 0, 27));
-
-        // System.out.println(VStats.computeOneMeanTTestHaLessThanValue(50, 50.3, 2.03,
-        // 100, 0.05));
-
-        // System.out.println(VStats.computeOneMeanTTestHaGreaterThanValue(55, 56, 2.03,
-        // 100, 0.05));
-
-        // System.out.println(VStats.computeOneMeanTTestHaNotEqualToValue(55, 54.7,
-        // 1.07, 100, 0.05));
-
-        // double[] indVar = { 4, 5, 7, 8, 55 };
-        // double[] depVar = { 7, 8, 9, 11, 12 };
-
-        // System.out.println(VStats.computeLinRegTTestUnequalSlope(indVar, depVar,
-        // 0.05));
-
-        // System.out.println(VStats.computeSeb(indVar, depVar));
-
-        // System.out.println(VStats.computeChiSquareCDF(1.02, 3, 5));
-
-        double[] observed = { 2, 4, 6 };
-        double[] expected = { 4, 4, 4 };
-
-        System.out.println(VStats.computeChiSquareGOFTest(observed, expected, 0.05));
+        // System.out.println(res);
 
     }
 }
--- Code/JavaCode/src/VCSV.java
+++ Code/JavaCode/src/VCSV.java
@@ -8,31 +8,9 @@ public class VCSV {
 
     }
 
-    // HELPER METHOD
-    private static int getNthIndexOf(String line, int n, String str) {
-        int count = 0;
-        ArrayList<Integer[]> tracker = new ArrayList<Integer[]>();
-
-        for (int i = 0; i < line.length(); i++) {
-            if (line.substring(i, i + 1).equals(str)) {
-                count++;
-                Integer[] arr = { count, i };
-                tracker.add(arr);
-            }
-        }
-
-        for (int i = 0; i < tracker.size(); i++) {
-            if (n == tracker.get(i)[0])
-                return tracker.get(i)[1];
-        }
-
-        return 0;
-    }
-
+    // GOOD
     public static int getNumCols(String fileName) {
-
         File file = new File(fileName);
-
         try {
             int count = 0;
             Scanner scanner = new Scanner(file);
@@ -49,46 +27,13 @@ public class VCSV {
     }
 
     // 0-based index logic
-    public static ArrayList<String> getColumnOfStrings(String fileName, int col) {
-
-        File file = new File(fileName);
-        ArrayList<String> res = new ArrayList<String>();
-
-        try {
-            Scanner scanner = new Scanner(file);
-            String line = scanner.nextLine();
-            line = scanner.nextLine();
-
-            while (line.length() != 0) {
-                if (col == 0)
-                    res.add((line.substring(0, line.indexOf(","))));
-                else if (col == getNumCols(fileName) - 1)
-                    res.add(line.substring(line.lastIndexOf(",") + 1));
-                else
-                    res.add(line.substring(getNthIndexOf(line, col, ",") + 1, getNthIndexOf(line,
-                            col + 1, ",")));
-                line = scanner.nextLine();
-            }
-
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-
-        return res;
-
-    }
-
-    // 0-based index logic
     public static double[] getColumnOfNumbers(String fileName, int col) {
-
         File file = new File(fileName);
         ArrayList<Double> res = new ArrayList<Double>();
-
         try {
             Scanner scanner = new Scanner(file);
             String line = scanner.nextLine();
             line = scanner.nextLine();
-
             while (line.length() != 0) {
                 if (col == 0)
                     res.add(Double.parseDouble(line.substring(0, line.indexOf(","))));
@@ -100,17 +45,32 @@ public class VCSV {
                                     1, ","))));
                 line = scanner.nextLine();
             }
-
         } catch (Exception e) {
             System.out.println(e);
         }
-
         double[] output = new double[res.size()];
         for (int i = 0; i < res.size(); i++) {
             output[i] = res.get(i);
         }
         return output;
+    }
 
+    // HELPER METHOD
+    private static int getNthIndexOf(String line, int n, String str) {
+        int count = 0;
+        ArrayList<Integer[]> tracker = new ArrayList<Integer[]>();
+        for (int i = 0; i < line.length(); i++) {
+            if (line.substring(i, i + 1).equals(str)) {
+                count++;
+                Integer[] arr = { count, i };
+                tracker.add(arr);
+            }
+        }
+        for (int i = 0; i < tracker.size(); i++) {
+            if (n == tracker.get(i)[0])
+                return tracker.get(i)[1];
+        }
+        return 0;
     }
 
 }
